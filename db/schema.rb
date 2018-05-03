a8b079cb0bdd7de399398e1ff5692de170b18ed1
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-05-03 19:50:25 +0200

    Add Icon to Index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412152926) do
+ActiveRecord::Schema.define(version: 20180503130358) do
 
   create_table "leagues", force: :cascade do |t|
     t.string "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20180412152926) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_players_on_email", unique: true
     t.index ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true
   end
